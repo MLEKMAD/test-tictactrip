7b670b112cb8fb742bf39690c29ac86829db96e8
LEKMAD <mohamed.lekamd@um5s.net.ma>
2021-04-08 01:46:40 +0200

    heroku conf

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.post("/api/justify", verifyToken, rateLimiter.middleware, (req, res) => {
 
 app.post("/api/token", authenticate);
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started on port 3000"));
 
 module.exports = app;
